eb85d7eb9f566d98ddba053081a53f878245e9d7
Ronny López <ronny@pricebets.com>
2012-06-26 21:32:18 +0200

    Added a connection registry to access connections.

---
+++ Connection/ConnectionRegistry.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lt\Bundle\RedisBundle\Connection;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ConnectionRegistry
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getConnection($name)
+    {
+        return $this->container->get('lt_redis.' .$name );
+    }
+}
